4e5dcb1951a1c5a24ed6aaacdffb4819fffa1624
fuzzything44 <tmccorma3776@boh285b49x.admin.bvsd.k12.co.us>
2015-08-24 14:44:00 -0600

    Set up for xCode

---
+++ Generator/Generator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Generator
+//
+//  Created by Tanner McCormack on 8/24/15.
+//  Copyright (c) 2015 Tanner McCormack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
