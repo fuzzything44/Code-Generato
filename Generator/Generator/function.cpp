32f51540f4911e473fe2b8d4432a99efbdb4565f
fuzzything44 <tmccorma3776@boh285b49x.admin.bvsd.k12.co.us>
2015-08-25 15:01:50 -0600

    Finish function.cpp

--- Generator/Generator/function.cpp
+++ Generator/Generator/function.cpp
@@ -1,7 +1,25 @@
 #include "function.h"
+using std::string;
+using std::vector;
 
-function::function(std::string name, std::vector<std::string>& templateArgs, std::vector<std::string>& args, std::string ret)
+function::function(std::string name, std::vector<std::string>& templateArgs, std::vector<std::string>& args, std::string ret) : name(name), templateArgs(templateArgs), args(args), ret(ret) {}
+
+vector<string> function::getArgs()
 {
-    
-    
+    return args;
+}
+
+string function::getName()
+{
+    return name;
+}
+
+string function::getRet()
+{
+    return ret;
+}
+
+vector<string> function::getTemplateArgs()
+{
+    return templateArgs;
 }
