00603267a6567c1779d86d5219e19c4743175069
Dr.Clue <twilight71777@gmail.com>
2015-08-26 13:38:42 -0600

    Mo names

--- Generator/Generator/variableNamer.cpp
+++ Generator/Generator/variableNamer.cpp
@@ -1,1 +1,55 @@
 #include "variableNamer.h"
+
+using namespace std;
+
+genName::genName(){
+    preCheck = false;
+    otherCheck = false;
+    int varNum = 0;
+    vector <string> name;
+    vector <string> name2;
+    
+    ifstream infile;
+    infile.open("names.txt");
+    string poop ;
+    
+    while(!infile.eof()){
+
+        
+        getline(infile,poop);
+        
+        
+        if( poop == "-Prefix")
+            preCheck = true;
+        
+        if(preCheck == true && poop!="-Prefix"){
+            
+            name.push_back(poop);
+    }
+        
+        if( poop == "-Other")
+            otherCheck = true;
+        
+        if(preCheck == true && poop!="-Other"){
+            
+            varNum ++;
+            name2.push_back(poop);
+        }
+        
+        
+        
+    }
+    infile.close();
+    preCheck = false;
+}
+
+string genName::getFunc(){
+    
+    string name;
+    randRange(1, 1);
+    
+    
+    
+    
+    return name;
+}
--- Generator/Generator/variableNamer.h
+++ Generator/Generator/variableNamer.h
@@ -1,1 +1,23 @@
 #pragma once
+#include"iostream"
+#include"random.h"
+#include"fstream"
+#include"string"
+
+class genName{
+public:
+    genName();
+    
+    std::string getVar();
+    std:: string getFunc();
+    ~genName();
+    
+private:
+    std::string poop;
+    std::vector <std::string> name;
+    std::vector <std::string> name2;
+    bool preCheck;
+    bool otherCheck;
+    int varNum;
+    int funNum;
+};
