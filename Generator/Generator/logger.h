0d2c96b87ea17365c617a81f51532cc1d1210fb9
fuzzything44 <tmccorma3776@boh285b49x.admin.bvsd.k12.co.us>
2015-08-31 14:35:34 -0600

    Logging ends line now

--- Generator/Generator/logger.h
+++ Generator/Generator/logger.h
@@ -11,7 +11,7 @@ namespace logger {
 
 #define LOG(ans) if (logger::log.is_open() ) { logger::init("default"); }               \
     try {                                                                               \
-        std::cout << "LOG: " << ans; logger::log << ans;                                \
+        std::cout << "LOG: " << ans << std::endl; logger::log << ans << std::endl;      \
     } catch(std::exception e)   {                                                       \
         std::cout << std::endl << "An error occurred: " << e.what() << std::endl;       \
         std::cout << "Attempted log: " << #ans << std::endl;                            \
@@ -19,7 +19,7 @@ namespace logger {
 
 #define CODE(ans) if (logger::code.is_open() ) { logger::init("default"); }             \
     try {                                                                               \
-        std::cout << "    #" << ans; logger::code << ans;                               \
+        std::cout << "    #" << ans << std::endl; logger::code << ans << std::endl;     \
     } catch(std::exception e)   {                                                       \
         std::cout << std::endl << "An error occurred: " << e.what() << std::endl;       \
         std::cout << "Attempted code: " << #ans << std::endl;                           \
