d6319f098b329bab1c69cbaf9402c671d3a22d27
fuzzything44 <slimesftw@gmail.com>
2015-09-15 14:42:38 -0600

    KILL PARENTS!

--- Generator/Generator/classScope.cpp
+++ Generator/Generator/classScope.cpp
@@ -18,11 +18,8 @@ classDef classScope::generate()
     classDef ret { name };
     
     // Decide if we will have parents.
-    if (randRange(0, 10) == 0 && types.size() > 0) {
-        // Determine what parents. Currently we just have one.
-        classDef parent = types[randRange(0, types.size() - 1)];
-        ret.addParent(parent);
-        CODE("class " << name << " : public " << parent.getName() )
+    if (randRange(0, 10) == 0 && types.size() > 0/* shouldn't be 0. Needs to be larger than base types */ && false /* to make it so we don't generate parents */) {
+        // Determine what parents. Currently we don't choose any.
     } else {
         CODE("class " << name)
     }
@@ -38,7 +35,7 @@ classDef classScope::generate()
     LOG("Creating private variables (not functions)" )
     for (int i = randRange(1, 10); i > 0; i--) {
         // Choose random type.
-        classDef type = types[randRange(0, types.size() - 1)];
+        classDef type = types[randRange(1, types.size() - 1)];
         
         // Choose name.
         string name = genName::get(type.getName());
@@ -65,7 +62,7 @@ classDef classScope::generate()
     // Create public variables.
     for (int i = randRange(0, 10); i > 0; i--) {
         // Create variable parameters.
-        classDef& type = types[randRange(0, types.size() - 1)];
+        classDef& type = types[randRange(1, types.size() - 1)];
         string varName = genName::get(type.getName() );
         
         // Create variable and add it.
