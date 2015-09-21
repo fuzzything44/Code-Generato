026bc50c88c374025ad81f3e56199865b27c6183
Dr.Clue <twilight71777@gmail.com>
2015-09-21 14:52:11 -0600

    FIx tmp

--- Generator/Generator/main.cpp
+++ Generator/Generator/main.cpp
@@ -14,13 +14,15 @@ int main() {
     logger::init("Name generator");
     genName::init();
     
-    
-
+    std::vector<classDef::variable> v;
+    for (int i = 0; i < 50; i++) {
+        cout << genName::get("typename", v) << endl;
+    }
     
 
         
-    globalNamespace g;
-    g.generate(3);
+ //   globalNamespace g;
+ //   g.generate(3);
     
     logger::close();
 }
